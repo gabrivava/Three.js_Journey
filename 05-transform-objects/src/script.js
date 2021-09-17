c906da7a2902beb972a4ceab11fc5247cafdbdaa
gabrivava <gabriele.vavassori4@gmail.com>
2021-09-17 13:56:36 +0200

    Group element and move group

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -12,31 +12,28 @@ const scene = new THREE.Scene()
  * 
  * Choose a unity method that match the project
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-/* mesh.position.x = 0.5
-   mesh.position.y = 0.5 
-   mesh.position.z = -2 */
-scene.add(mesh)
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 2
+scene.add(group)
 
-/* Update all mesh coordinate at once */
-mesh.position.set(0.7, 0.6, 1)
-
-
-// Scale
-mesh.scale.x = 2
-mesh.scale.set(2, 0.5, 0.5)
-
-
-// Rotation (rotation and quaternion) [make reorder before changing the locatiom]
-mesh.rotation.reorder('YXZ')
-mesh.rotation.y = Math.PI * 0.25
-mesh.rotation.x = Math.PI * 0.25
-
-// Quaternion (rotation in a matematial way)
-
-
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'red' })
+)
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'blue' })
+)
+cube2.position.x = -2
+group.add(cube2)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'green' })
+)
+cube3.position.x = 2
+group.add(cube3)
 // Axes helper (argoment to pass is the units to display)
 const axesHelper = new THREE.AxesHelper(3)
 scene.add(axesHelper);
@@ -54,21 +51,8 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3.5
-camera.position.y = 1
-camera.position.x = 1
+camera.position.z = 5
 scene.add(camera)
-
-// Look at (give a vector3)
-camera.lookAt(mesh.position)
-console.log(camera.rotation);
-
-/* Normalize */
-/* console.log(camera.position.length());
-camera.position.normalize()
-console.log(camera.position.length()); */
-
-// console.log(mesh.position.distanceTo(camera.position));
 
 /**
  * Renderer
