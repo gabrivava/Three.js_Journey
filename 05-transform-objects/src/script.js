a6da326f8afdc8c6916362e2bf3dd9f7cc2ec187
gabrivava <gabriele.vavassori4@gmail.com>
2021-09-17 13:47:44 +0200

    basic of  position, scale, rotation + normalize and look at

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,11 +9,38 @@ const scene = new THREE.Scene()
 
 /**
  * Objects
+ * 
+ * Choose a unity method that match the project
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+/* mesh.position.x = 0.5
+   mesh.position.y = 0.5 
+   mesh.position.z = -2 */
 scene.add(mesh)
+
+/* Update all mesh coordinate at once */
+mesh.position.set(0.7, 0.6, 1)
+
+
+// Scale
+mesh.scale.x = 2
+mesh.scale.set(2, 0.5, 0.5)
+
+
+// Rotation (rotation and quaternion) [make reorder before changing the locatiom]
+mesh.rotation.reorder('YXZ')
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
+
+// Quaternion (rotation in a matematial way)
+
+
+// Axes helper (argoment to pass is the units to display)
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper);
+
 
 /**
  * Sizes
@@ -27,8 +54,21 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.z = 3.5
+camera.position.y = 1
+camera.position.x = 1
 scene.add(camera)
+
+// Look at (give a vector3)
+camera.lookAt(mesh.position)
+console.log(camera.rotation);
+
+/* Normalize */
+/* console.log(camera.position.length());
+camera.position.normalize()
+console.log(camera.position.length()); */
+
+// console.log(mesh.position.distanceTo(camera.position));
 
 /**
  * Renderer
