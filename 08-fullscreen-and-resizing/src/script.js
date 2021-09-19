9187e3dafc64e24c13c7057bb0baf76e785deb5e
gabrivava <gabriele.vavassori4@gmail.com>
2021-09-19 10:21:43 +0200

    Resize window adn update size,camera and renderer

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -27,6 +27,21 @@ const sizes = {
     height: window.innerHeight
 }
 
+window.addEventListener('resize', () => 
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+
+})
+
 /**
  * Camera
  */
