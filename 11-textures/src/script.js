80bfee138aba2a04c63f0f98a1c61c496f765b3e
gabrivava <gabriele.vavassori4@gmail.com>
2021-09-29 19:49:25 +0200

    load progress and make many texture

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -6,22 +6,34 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
  * Texture
  */
 const loadingManager = new THREE.LoadingManager()
+
+loadingManager.onStart = () =>
+{
+    console.log('onStart');
+}
+loadingManager.onLoad = () =>
+{
+    console.log('onLoad');
+}
+loadingManager.onProgress = () =>
+{
+    console.log('onProgress');
+}
+loadingManager.onError = () =>
+{
+    console.log('onError');
+}
+
 const textureLoader = new THREE.TextureLoader(loadingManager)
-const texture = textureLoader.load(
-    '/textures/door/color.jpg',
-    () =>
-    {
-        console.log('load');
-    },
-    () =>
-    {0
-        console.log('progress');
-    },
-    () =>
-    {
-        console.log('error');
-    }
-    )
+const colorTexture = textureLoader.load('/textures/door/color.jpg')  
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')    
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')    
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')    
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')    
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')    
+
+
 
 /**
  * Base
@@ -36,7 +48,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ map: texture })
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
