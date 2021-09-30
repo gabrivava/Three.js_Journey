df774d05a7331c15d0bb2d6ff863c986cabc0a58
gabrivava <gabriele.vavassori4@gmail.com>
2021-09-30 09:32:20 +0200

    Add 3 object to the scene(sphere, plane, torus)

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -12,6 +12,34 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Objects
+ */
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+
+const sphere = new THREE.Mesh(
+    // geometry
+    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    // material
+    material
+)
+sphere.position.x = -1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1, 1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusBufferGeometry(0.3, 0.2, 16, 32),
+    material
+)
+torus.position.x = 1.5
+
+// scene.add(sphere)
+// scene.add(plane)
+scene.add(sphere, plane, torus)
+
+/**
  * Sizes
  */
 const sizes = {
