2ebfcc07ec3a3bc76fa05b763a10338b6b949ed3
gabrivava <gabriele.vavassori4@gmail.com>
2021-10-02 20:45:46 +0200

    create inital text using typeface font

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -21,14 +21,46 @@ const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 
 /**
+ * Fonts
+ */
+const fontLoader = new THREE.FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        const textGeometry = new THREE.TextBufferGeometry(
+            // text to show
+            'Gabriele Vavassori',
+            // parameters
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+
+        const textMaterial = new THREE.MeshBasicMaterial()
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+
+        scene.add(text)
+    }
+)
+
+/**
  * Object
  */
-const cube = new THREE.Mesh(
+/* const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial()
 )
 
-scene.add(cube)
+scene.add(cube) */
 
 /**
  * Sizes
