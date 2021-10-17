76573783dbee8e055400a589874f5ab28b6cff32
gabrivava <gabriele.vavassori4@gmail.com>
2021-10-17 10:28:32 +0200

    Casting shadow and resize them for better performace

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -172,6 +172,7 @@ for (let i = 0; i < 50; i++)
     grave.position.set(x, 0.1, z)
     grave.rotation.y = Math.random() * 5
     grave.rotation.x = Math.random() / 2
+    grave.castShadow = true
     graves.add(grave)
 }
 
@@ -253,6 +254,44 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setClearColor('#262837')
+
+/**
+ * Shadows
+ */
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+moonLight.castShadow = true
+doorLight.castShadow = true
+ghost1.castShadow = true
+ghost2.castShadow = true
+ghost3.castShadow = true
+
+walls.castShadow = true
+// roof.castShadow = true
+bush1.castShadow = true
+bush2.castShadow = true
+bush3.castShadow = true
+bush4.castShadow = true
+
+floor.receiveShadow = true
+
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 7
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 7
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 7
+
 
 /**
  * Animate
